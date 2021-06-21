ddb9ea3432f78a8ee3007615fa250e548d081646
TenzingS <thesherpatenzing@gmail.com>
2021-06-21 03:15:52 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.getElementById('searchByID');
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('#movieDetails h4');
+            // Best to use query selector when trying to specify elements inside IDs (h4 and p in this case)
+            const summary = document.querySelector('#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
